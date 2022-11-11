c74ca9672cc6c8923f876fd45c142dcf70c6ebfb
Fayemi praise bisola <114654549+Debby25@users.noreply.github.com>
2022-11-11 17:14:04 +0100

    Add files via upload

---
+++ SLIDESHOW/slide.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelector("[data-carousel-button]")
+
+buttons.forEach(button => {
+    button.addEventlistener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button
+         .closest("[data-carousel")
+         .querySelector("[data-slides")
+    const activeSlides = slides.querySelector("[data-active")
+    let newIndex =[...slides.children].indexOf(activeSlides) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlides.dataset.active
+    })   
+})
